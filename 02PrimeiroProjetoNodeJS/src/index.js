294125e6977e441d837a119009243ac7e3a5a183
Deibson Cogo <deibsoncogo@outlook.com>
2021-08-22 18:03:31 -0300

    Aula 12 / Cadastro de conta

--- 02PrimeiroProjetoNodeJS/src/index.js
+++ 02PrimeiroProjetoNodeJS/src/index.js
@@ -1,5 +1,26 @@
-const express = require("express");
+const express = require("express"); // realiza a importação da dependência
+const { v4: uuidV4 } = require("uuid"); // realiza a importação da dependência e renomeia ela
 
-const app = express();
+const app = express(); // método que ajuda na utilização do express
 
+app.use(express.json()); // configura o tipo de dados a ser trabalhado
+
+const customers = []; // cria um banco de dados volátil
+
+app.post("/account", (request, response) => { // cria uma conta
+  const { cpf, name } = request.body; // recebe as informações passada pela requisição
+
+  const customer = { // prepara os sados a salvar
+    id: uuidV4(),
+    cpf,
+    name,
+    statement: [],
+  };
+
+  customers.push(customer); // salva os dados
+
+  return response.status(201).json(customer); // retorna algo ao usuário
+});
+
+// define a porta de execução do servidor
 app.listen(3333, () => console.log("Server running on port 3333!"));
