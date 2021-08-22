a033a630ba29cc27e6b97484ce908c3f2604d65c
Deibson Cogo <deibsoncogo@outlook.com>
2021-08-22 17:15:45 -0300

    Aula 10 / Tipos de parâmetros

--- 01FundamentosNodeJS/src/index.js
+++ 01FundamentosNodeJS/src/index.js
@@ -2,24 +2,47 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json()); // define o formato dos arquivos a receber
+
 app.post("/course", (request, response) => { // método para criar algo
-  return response.json(["Curso 1", "Curso 2", "Curso 3", "Curso 4"]);
+  // recebe a informação dentro da requisição
+  const { name } = request.body;
+
+  // retorna algo ao usuário
+  return response.json(["Curso 1", "Curso 2", "Curso 3", "Curso 4", name]);
 });
 
 app.get("/course", (request, response) => { // método para realizar consulta
-  return response.json(["Curso 1", "Curso 2", "Curso 3"]);
+  // recebe a informação pelo link do endereço (http://localhost:3000/user?page=2&order=desc)
+  const { page, order } = request.query;
+
+  // retorna algo ao usuário
+  return response.json(["Curso 1", "Curso 2", "Curso 3", page, order]);
 });
 
 app.put("/course/:id", (request, response) => { // método para alterar várias coisas
-  return response.json(["Curso 5", "Curso 6", "Curso 3", "Curso 4"]);
+  // recebe a informação pelo link do endereço (http://localhost:3000/user/123)
+  const { id } = request.params;
+
+  // retorna algo ao usuário
+  return response.json(["Curso 5", "Curso 6", "Curso 3", "Curso 4", id]);
 });
 
 app.patch("/course/:id", (request, response) => { // método para alterar uma coisa
-  return response.json(["Curso 5", "Curso 6", "Curso 7", "Curso 4"]);
+  // recebe a informação pelo link do endereço (http://localhost:3000/user/4567)
+  const { id } = request.params;
+
+  // retorna algo ao usuário
+  return response.json(["Curso 5", "Curso 6", "Curso 7", "Curso 4", id]);
 });
 
 app.delete("/course/:id", (request, response) => { // método para deletar algo
-  return response.json(["Curso 5", "Curso 6", "Curso 7"]);
+  // recebe a informação pelo link do endereço (http://localhost:3000/user/890)
+  const { id } = request.params;
+
+  // retorna algo ao usuário
+  return response.json(["Curso 5", "Curso 6", "Curso 7", id]);
 });
 
+// define a porta de execução do servidor
 app.listen(3333, () => console.log("Server running on port 3333!"));
