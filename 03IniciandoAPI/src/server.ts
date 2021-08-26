6d9af5c73d4965dc61f42c296afca46f59ae660f
Deibson Cogo <deibsoncogo@outlook.com>
2021-08-26 20:48:09 -0300

    Aula 24 / Criando um projeto com o TypeScript

---
+++ 03IniciandoAPI/src/server.ts
@@ -0,0 +1,9 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (request, response) => {
+  return response.json({ message: "Hello word!" });
+});
+
+app.listen(3333, () => console.log("Server running on port 3333!"));
