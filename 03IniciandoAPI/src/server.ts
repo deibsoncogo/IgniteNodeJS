d88ab96abd4fca7192bde5c2f3b07f1770791329
Deibson Cogo <deibsoncogo@outlook.com>
2021-08-26 21:54:44 -0300

    Aula 27 / Configurando o TS-NODE-DEV

--- 03IniciandoAPI/src/server.ts
+++ 03IniciandoAPI/src/server.ts
@@ -1,9 +1,10 @@
 import express from "express"; // realiza a importação da dependência
-import CreateCourse from "./route"; // realiza a importação do arquivo rota
 
 const app = express(); // método para facilitar o uso
 
-app.get("/", CreateCourse); // definição de uma rota
+app.get("/", (request, response) => { // rota de teste
+  return response.json({ message: "Hello world!" });
+});
 
 // define a porta de execução do servidor
-app.listen(3333, () => console.log("Server running on port 3333!"));
+app.listen(3333, () => console.log("Server is running on port 3333!"));
