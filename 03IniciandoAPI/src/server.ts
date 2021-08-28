22adcd61852db29ee7eb137474e7e8c358262684
Deibson Cogo <deibsoncogo@outlook.com>
2021-08-27 21:31:59 -0300

    Aula 28 / Debugando a aplicação

--- 03IniciandoAPI/src/server.ts
+++ 03IniciandoAPI/src/server.ts
@@ -2,9 +2,17 @@ import express from "express"; // realiza a importação da dependência
 
 const app = express(); // método para facilitar o uso
 
+app.use(express.json()); // define o formato dos dados utilizado no request e response
+
+app.post("/course", (request, response) => {
+  const { name } = request.body;
+
+  return response.status(201).json(name);
+});
+
 app.get("/", (request, response) => { // rota de teste
   return response.json({ message: "Hello world!" });
 });
 
 // define a porta de execução do servidor
-app.listen(3333, () => console.log("Server is running on port 3333!"));
+app.listen(3333, () => console.log("Server is running on port 3333"));
