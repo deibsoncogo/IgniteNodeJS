d6d618ff993e8008fdffba99ee579d5344f5c091
Deibson Cogo <deibsoncogo@outlook.com>
2021-08-29 17:20:31 -0300

    Aula 30 / Inserindo ID com o uuid

--- 03IniciandoAPI/src/routes/categoryRoute.ts
+++ 03IniciandoAPI/src/routes/categoryRoute.ts
@@ -1,13 +1,14 @@
 import { Router } from "express"; // importa a dependência sobre rota
+import { v4 as uuidV4 } from "uuid";
 
 const categoryRoute = Router(); // método que ajuda na programação
 
 const categories = []; // banco de dados volátil
 
-categoryRoute.post("/category", (request, response) => { // cria uma categoria
+categoryRoute.post("/", (request, response) => { // cria uma categoria
   const { name, description } = request.body; // recebe os dados dentro da requisição
 
-  const category = { name, description }; // prepara os dados antes de salvar
+  const category = { id: uuidV4(), name, description }; // prepara os dados antes de salvar
 
   categories.push(category); // salva os dados dentro do banco de dados
 
--- 03IniciandoAPI/src/server.ts
+++ 03IniciandoAPI/src/server.ts
@@ -5,7 +5,7 @@ const app = express(); // método para facilitar o uso
 
 app.use(express.json()); // define o formato dos dados utilizado no request e response
 
-app.use(categoryRoute); // cria uma rota para as categorias
+app.use("/category", categoryRoute); // cria uma rota para as categorias
 
 // define a porta de execução do servidor
 app.listen(3333, () => console.log("Server is running on port 3333"));
