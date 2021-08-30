58202876472874a893e419c30fc6af9d4dd98e44
Deibson Cogo <deibsoncogo@outlook.com>
2021-08-29 21:42:24 -0300

    Aula 36 / Utilizando o princípio de responsabilidade única (SRP)

--- 03IniciandoAPI/src/routes/categoryRoute.ts
+++ 03IniciandoAPI/src/routes/categoryRoute.ts
@@ -1,5 +1,6 @@
 import { Router } from "express"; // importa a dependência sobre rota
 import { CategoryRepository } from "../repositories/CategoryRepository"; // importa o arquivo criado
+import { CreateCategoryService } from "../services/createCategoryService"; // importa o arquivo criado
 
 const categoryRoute = Router(); // método que ajuda na programação
 const categoryRepository = new CategoryRepository(); // instancia para poder utilizar o arquivo
@@ -7,13 +8,10 @@ const categoryRepository = new CategoryRepository(); // instancia para poder uti
 categoryRoute.post("/", (request, response) => { // cria uma categoria
   const { name, description } = request.body; // recebe os dados dentro da requisição
 
-  const categoryAlreadyExists = categoryRepository.findByName(name); // chama a função
+  // instancia para poder utilizar todos recursos criando um vinculo com o repositório
+  const createCategoryService = new CreateCategoryService(categoryRepository);
 
-  if (categoryAlreadyExists) { // evita a duplicação do nome de categoria
-    return response.status(400).json({ error: "Já existe uma categoria com este nome" });
-  }
-
-  const category = categoryRepository.create({ name, description }); // chama a função
+  const category = createCategoryService.execute({ name, description }); // chama a função
 
   return response.status(201).json(category); // retornar algo ao chamado
 });
---
+++ 03IniciandoAPI/src/services/createCategoryService.ts
@@ -0,0 +1,25 @@
+import { CategoryModel } from "../model/categoryModel"; // importação do model de categoria
+import { CategoryRepository } from "../repositories/CategoryRepository"; // importação do repositório de categoria
+
+interface IRequest { // tipagem dos itens a receber pelo request
+  name: string;
+  description: string;
+}
+
+class CreateCategoryService { // grupo único e principal
+  constructor(private categoryRepository: CategoryRepository) {} // criar o acesso ao repositório
+
+  execute({ name, description }: IRequest): CategoryModel { // função única e principal
+    const categoryAlreadyExists = this.categoryRepository.findByName(name); // chama a função
+
+    if (categoryAlreadyExists) { // evita a duplicação do nome de categoria
+      throw new Error("Já existe uma categoria com este nome"); // chama a função de erro
+    }
+
+    const category = this.categoryRepository.create({ name, description }); // chama a função
+
+    return category; // retorna algo ao chamador
+  }
+}
+
+export { CreateCategoryService }; // exporta tudo para ser utilizado em outro arquivo
