191de0e81881f78e5a01adc905f52d55804c4d37
Deibson Cogo <deibsoncogo@outlook.com>
2021-09-04 20:41:35 -0300

    Aula 48 / Criando upload de arquivos

--- 03IniciandoAPI/src/routes/categoryRoute.ts
+++ 03IniciandoAPI/src/routes/categoryRoute.ts
@@ -1,8 +1,13 @@
 import { Router } from "express"; // importa a dependência sobre rota
+import multer from "multer"; // dependencia que lida com importações csv
 import { createCategoryController } from "../modules/cars/useCases/createCategory";
 import { listCategoryController } from "../modules/cars/useCases/listCategory";
 
 const categoryRoute = Router(); // método que ajuda na programação
+
+const upload = multer({ // método para configurar e ajudar na utilização do mesmo
+  dest: "./tmp", // local de salvamento do arquivo recebido
+});
 
 categoryRoute.post("/", (request, response) => { // cria uma categoria
   return createCategoryController.execute(request, response); // chama o controlador responsável
@@ -10,6 +15,12 @@ categoryRoute.post("/", (request, response) => { // cria uma categoria
 
 categoryRoute.get("/", (request, response) => { // lista as categorias
   return listCategoryController.execute(request, response); // chama o controlador responsável
+});
+
+categoryRoute.post("/import", upload.single("file"), (request, response) => { // importação de categoria
+  const { file } = request; // recebe o arquivo enviado
+
+  return response.json(file); // retorna algo ao chamador
 });
 
 export { categoryRoute }; // exporta todo o conteúdo para poder ser utilizado em outro arquivo
