d305533b3ba010b02449bb2e09c0f34216dce922
Deibson Cogo <deibsoncogo@outlook.com>
2021-09-26 19:17:34 -0300

    Transformando os containers composer em normal

--- 03IniciandoAPI/src/database/index.ts
+++ 03IniciandoAPI/src/database/index.ts
@@ -1,13 +1,4 @@
-import { createConnection, getConnectionOptions } from "typeorm"; // importa o ORM que iremos usar
+import { createConnection } from "typeorm"; // importa o ORM que iremos usar
 
-interface IOptions { // definição dos dados recebido
-  host: string;
-}
-
-// função para lidar com a conexão
-getConnectionOptions().then((options) => {
-  const newOptions = options as IOptions;
-  newOptions.host = "database";
-  // criação a conexão (Model/Entity <--> ORM <--> Banco de dados)
-  createConnection({ ...options });
-});
+// criação a conexão (Model <--> ORM <--> Banco de dados)
+createConnection();
