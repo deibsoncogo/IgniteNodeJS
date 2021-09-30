00b1da25352e0a4923e5ba47aea0270aa4a5c2c7
Deibson Cogo <deibsoncogo@outlook.com>
2021-09-29 23:15:37 -0300

    Aula 92 / Teste de autenticação do usuário

---
+++ 03IniciandoAPI/tests/modules/accountrs/repositories/userRepositoryInMemory.ts
@@ -0,0 +1,31 @@
+import { ICreateUserDto } from "../../../../src/modules/accountrs/dtos/iCreateUserDto"; // importação das tipagens de DTO
+import { UserEntity } from "../../../../src/modules/accountrs/entities/userEntity"; // importação das entidades
+import { IUserRepository } from "../../../../src/modules/accountrs/repositories/iUserRepository"; // importação do contrato do repositório
+
+class UserRepositoryInMemory implements IUserRepository { // classe principal
+  users: UserEntity[] = []; // banco de dados volatil
+
+  async create({ name, email, password, driverLicense }: ICreateUserDto): Promise<UserEntity> { // cria um usuário
+    const user = new UserEntity(); // instancia para criar o banco de dados
+
+    Object.assign(user, { name, email, password, driverLicense }); // prepara os dados antes de salvar
+
+    this.users.push(user); // salva os dados
+
+    return user; // retornar algo ao chamador
+  }
+
+  async findByEmail(email: string): Promise<UserEntity> { // busca um usuário pelo email
+    const userFind = this.users.find((user) => user.email === email); // realiza a busca
+
+    return userFind; // retorna algo ao chamador
+  }
+
+  async findById(id: string): Promise<UserEntity> { // busca um usuário pelo id
+    const userFind = this.users.find((user) => user.id === id); // realiza a busca
+
+    return userFind; // retorna algo ao chamador
+  }
+}
+
+export { UserRepositoryInMemory }; // exporta para poder ser chamado
---
+++ 03IniciandoAPI/tests/modules/accountrs/useCases/authenticateUserService.spec.ts
@@ -0,0 +1,59 @@
+import "reflect-metadata"; // importa a dependencia necessária para o tsyringe funcionar
+import { AppError } from "../../../../src/errors/appError"; // importa o arquivo gerenciador de erros
+import { ICreateUserDto } from "../../../../src/modules/accountrs/dtos/iCreateUserDto"; // importa a tipagem de criação do usuário DTO
+import { AuthenticateUserService } from "../../../../src/modules/accountrs/useCases/authenticateUser/authenticateUserService"; // importa o serviço de autenticação
+import { CreateUserService } from "../../../../src/modules/accountrs/useCases/createUser/createUserService"; // importa o serviço do usuário
+import { UserRepositoryInMemory } from "../repositories/userRepositoryInMemory"; // importa o repositório volatil
+
+let authenticateUserService: AuthenticateUserService; // cria a variavel para o serviço
+let userRepositoryInMemory: UserRepositoryInMemory; // cria a variavel para o repositório
+let createUserService: CreateUserService; // cria a variavel para o serviço
+
+describe("Autenticação do usuário", () => { // cria um grupo para os testes
+  beforeEach(() => { // serve para executar um grupo de comando antes de todos os testes
+    userRepositoryInMemory = new UserRepositoryInMemory(); // instancia para criar o acesso ao repositório
+    authenticateUserService = new AuthenticateUserService(userRepositoryInMemory); // instancia para criar o acesso ao serviço com o repositório vinculado
+    createUserService = new CreateUserService(userRepositoryInMemory); // instancia para criar o acesso ao repositório
+  });
+
+  it("O usuário deve ser capaz de se autenticar", async () => { // cria um teste
+    const { name, email, password, driverLicense }: ICreateUserDto = { // dados do usuário
+      name: "User test",
+      email: "user@test.com.br",
+      password: "123456",
+      driverLicense: "000123",
+    };
+
+    // chama o servico de criação do usuário
+    await createUserService.execute({ name, email, password, driverLicense });
+
+    // chama o servico de criação da autenticação
+    const token = await authenticateUserService.execute({ email, password });
+
+    expect(token).toHaveProperty("token"); // verifica se existe o campo token
+  });
+
+  it("Não deve ser capaz de autenticar um usuário inexistente", () => { // cria o teste
+    expect(async () => { // define oque olhar
+      // chama o servico de criação da autenticação
+      await authenticateUserService.execute({ email: "user@test.com.br", password: "123456" });
+    }).rejects.toBeInstanceOf(AppError); // verifica se lançou um erro do arquivo AppError
+  });
+
+  it("Não deve ser capaz de autenticar um usuário com a senha incorreta", () => { // cria o teste
+    expect(async () => { // define oque olhar
+      const { name, email, password, driverLicense }: ICreateUserDto = { // dados do usuário
+        name: "User test",
+        email: "user@test.com.br",
+        password: "123456",
+        driverLicense: "000123",
+      };
+
+      // chama o serviço de criação do usuário
+      await createUserService.execute({ name, email, password, driverLicense });
+
+      // chama o serviço de criação de autenticação do usuário
+      await authenticateUserService.execute({ email, password: "incorrectPassword" });
+    }).rejects.toBeInstanceOf(AppError); // verifica se lançou um erro do arquivo AppError
+  });
+});
